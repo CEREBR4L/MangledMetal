51c6b330da6dab139e20f03c2a3f99d6a5bd7af3
CEREBR4L <mattmillergraphics@gmail.com>
2016-10-15 21:56:13 +0100

    Tweeter amendments

--- tweeter.js
+++ tweeter.js
@@ -8,7 +8,7 @@ var quotesDB = require('./quotes/functions.js');
        
 /* config */
 var connect = mongoose.connect('mongodb://127.0.0.1:27017/MangledMetal');
-var port = process.env.PORT || 8082;
+var port = process.env.PORT || 82;
 
 env(__dirname + '/.env');
 
@@ -46,7 +46,7 @@ function postTweet(){
 
 	});
 
-	setTimeout(postTweet, 1000 * 60 * 60);
+	setTimeout(postTweet, 1000 * 60 * 30);
 
 }
 
