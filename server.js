eb227fcf232bfa57ab4c78c44bcf0fa5a49da7c1
CEREBR4L <mattmillergraphics@gmail.com>
2016-10-15 23:50:12 +0100

    I have a face now

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ var port = process.env.PORT || 8081;
 
 app.get('/quotes', quotesDB.getAll);
 
+/* public site */
+app.use('/css/', express.static(__dirname + '/myFace/css/'));
+app.use('/img/', express.static(__dirname + '/myFace/img/'));
+
+app.use('*', function(req, res){
+  res.sendFile(__dirname + '/myFace/');
+});
+
 app.listen(port, function(){
     console.log("App running on: " + port);
 });
